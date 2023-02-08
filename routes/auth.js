0a0d5dc3e90e20f4860ab34549ad59451c217526
Marisol Cisneros <mcisnerosrangel@gmail.com>
2023-02-08 12:24:53 -0600

    Terminamos back uts

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,9 @@ const router = require('express').Router()
 const User = require('../models/User')
 const Joi = require('@hapi/joi')
 const bcrypt = require('bcrypt')
-const Jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 
-const {Schema, schema} = require('../models/User')
+//const {Schema, schema} = require('../models/User')
 
 const SchemaRegister = Joi.object({
     name: Joi.string().min(6).max(255).required(),
@@ -12,11 +12,20 @@ const SchemaRegister = Joi.object({
     email: Joi.string().max(1024).required(),
     password: Joi.string().min(6).required()
 })
+
+const schemaUpdateuser = Joi.object({
+    id:Joi.string().max(255).required(),
+    name: Joi.string().min(6).max(255).required(),
+    lastname: Joi.string().max(255).required(),
+    email: Joi.string().max(1024).required(),
+    password: Joi.string().min(6).required()
+})
+
 const SchemaLogin = Joi.object({
     email: Joi.string().max(1024).required(),
     password: Joi.string().min(6).required()
 })
-router.post('/registar', async(req, res)=>{
+router.post('/register', async(req, res)=>{
     //validación de usuario
     const{error} = SchemaRegister.validate(req.body)
 if(error){
@@ -35,32 +44,29 @@ if(isEmailUnique){
     
 }
 const salt = await bcrypt.genSalt(10)
-const passwordEncriptado = await bcrypt.hash(req.password,salt)
+const passwordEncriptado = await bcrypt.hash(req.body.password, salt)
 
-    const USUARIO = new User({
+    const usuario = new User({
         name: req.body.name,
         lastname:req.body.lastname,
         email: req.body.email,
-        pasword:req.body.passwordEncriptado,
+        password: passwordEncriptado
     })
     try{
        // const guardado = await USUARIO.save()
-        const actualizado = await User.findByIdAndDelete(req.body.id,USUARIO)
+        const guardado = await usuario.save()
         res.json({
-        message:'Success Update',
-        data:actualizado
+        message:'Success',
+        data:guardado
         })
 
     }catch(error){
         res.status(400).json({
-            message:'Error alActualizar',
+            message:'Error al guardar',
             error
         })
     }
-    res.json({
-        error:null,
-        data:'Aqui vamos a poner los datos '
-    })
+
 })
 
 
@@ -71,10 +77,10 @@ router.post('/login', async(req,res)=> {
             error:error.details[0].message
         })
     }
-        const isEmailUnique = await User.findOne({ email: req.body.ema })
+        const isEmailUnique = await User.findOne({ email: req.body.email })
         if(!isEmailUnique){
-            returnres.status(400).json({
-                error:"El correo ya existe"
+            return res.status(400).json({
+                error:"El correo no existe"
             })
         }
         const validarPassword = await bcrypt.compare(req.body.password, isEmailUnique.password)
@@ -83,16 +89,16 @@ router.post('/login', async(req,res)=> {
                 error: "password Incorrecto"
             })
         }
-        const token = Jwt.sign({
-            name:isEmailUnique._name,
-            id:isEmailUnique._id
-        },process.env.TOKEN_SECRET)
+        const token = jwt.sign({
+            name: isEmailUnique.name,
+            id: isEmailUnique._id
+        }, process.env.TOKEN_SECRET)
         res.header('auth-token', token).json({
             error:null,
-            data:{token }
+            data:{ token }
         })
         })
-
+//llamar usuarios
         router.get('/getallusers',async(req,res)=>{
             const users = await User.find()
 
@@ -107,7 +113,7 @@ router.post('/login', async(req,res)=> {
                 })
             }
         })
-
+//erase borrar usuario
         router.post('/eraseuser',async(req,res)=>{
             const id= req.body.id
 
@@ -125,68 +131,8 @@ router.post('/login', async(req,res)=> {
         
             }
         })
-        router.post('/login', async(req,res)=> {
-    const{error}= SchemaLogin.validate(req.body)
-    if(error){
-        return res.status(400).json({
-            error:error.details[0].message
-        })
-        
-    }
-        const isEmailUnique = await User.findOne({ email: req.body.ema })
-        if(!isEmailUnique){
-            returnres.status(400).json({
-                error:"El correo ya existe"
-            })
-        }
-        const validarPassword = await bcrypt.compare(req.body.password, isEmailUnique.password)
-        if(!validarPassword){
-            return res.status(400).json({
-                error: "password Incorrecto"
-            })
-        }
-        const token = Jwt.sign({
-            name:isEmailUnique._name,
-            id:isEmailUnique._id
-        },process.env.TOKEN_SECRET)
-        res.header('auth-token', token).json({
-            error:null,
-            data:{token }
-        })
-        })
-
-        router.get('/getallusers',async(req,res)=>{
-            const users = await User.find()
-
-            if(users){
-                res.json({
-                    error:null,
-                    data:users
-                })
-            }else{
-                return res.status(400).json({
-                    error:"No hay usuarios"
-                })
-            }
-        })
-
-        router.post('/eraseuser',async(req,res)=>{
-            const id= req.body.id
-
-
-            const erased = await User.findByIdAndDelete(id)
-            if(erased){
-                res.json({
-                    error:null,
-                    message:"Borrado satisfactoriamente"
-                })
-            }else{
-                return res.status(400).json({
-                    error:"No se pudo borrar el usuario"
-                })
-        
-            }
-        })
+      
+    
         //////////Modificar///////////
        
         router.post('/updateuser', async(req, res) => {
@@ -223,7 +169,7 @@ router.post('/login', async(req,res)=> {
             //})
         
             try{
-                const actualizar = await User.findByIdAndUpdate(req.body.id, usuario)
+                const actualizar = await User.findByIdAndUpdate(req.body.id, usuario, {new:true})
                 res.json({
                     message: 'Success',
                     data: actualizar
